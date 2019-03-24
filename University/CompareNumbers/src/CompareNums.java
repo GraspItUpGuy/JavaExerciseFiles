91e918eb84be2cb1f31bc3b48549d2e3dc546152
Puneet <birmaan.puneet19@gmail.com>
2019-03-25 05:27:41 +0530

    number comparison
    Signed-off-by: Puneet <birmaan.puneet19@gmail.com>

--- University/CompareNumbers/src/CompareNums.java
+++ University/CompareNumbers/src/CompareNums.java
@@ -1,2 +1,21 @@
+import java.util.Scanner;
 public class CompareNums {
+    public static void main(String[] args){
+
+  Scanner input = new Scanner(System.in);
+
+          int num1, num2 ;
+        System.out.println("enter number 1 : ");
+        num1 = input.nextInt();
+        System.out.println("enter number 2 : ");
+        num2 = input.nextInt();
+
+        if(num1 > num2){
+            System.out.println("Num1 is greater");
+        }else if (num1 < num2){
+            System.out.println("num2 is greater");
+        }else if(num1 == num2){
+            System.out.println("numbers are equal");
+        }
+    }
 }
