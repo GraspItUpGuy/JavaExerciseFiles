c1c932aa445a2b5d8491d4d49f0ffea924bc033b
Puneet <birmaan.puneet19@gmail.com>
2019-03-25 03:50:43 +0530

    hello world
    Signed-off-by: Puneet <birmaan.puneet19@gmail.com>

--- Basics/Hello-World/src/Hello.java
+++ Basics/Hello-World/src/Hello.java
@@ -1,10 +1,5 @@
 public class Hello {
-    public static  void main(String[] args){
+    public static void main(String[] args){
         System.out.println("Hello World");
-        System.out.println("Hello Java");
-        System.out.println("Hello You");
-           // println for next line, print for printing in same line
-
-
     }
 }
--- University/Progs/randomnums/src/randomnums.java
+++
@@ -1,6 +0,0 @@
-public class randomnums {
-    public static  void main(String[] args){
-       for (int i = 1; i<=6; i++) {
-           System.out.println( (int)(Math.random() *100));}
-    }
-}
