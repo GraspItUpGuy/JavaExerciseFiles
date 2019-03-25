9b2128d244b0d2759b5aaa957cff69f355e5eb9f
Puneet <birmaan.puneet19@gmail.com>
2019-03-25 15:57:31 +0530

    star pattern 2
    Signed-off-by: Puneet <birmaan.puneet19@gmail.com>

--- University/Star1/src/StarPattern.java
+++ University/Star1/src/StarPattern.java
@@ -1,10 +1,24 @@
 public class StarPattern {
     public static void main(String[] args){
+        // Pattern 1
+        System.out.println("Pattern 1");
         for(int i=0; i<5 ; i++){
            for (int j=0; j<=i; j++){
                System.out.print("*");
            }
             System.out.println("");
         }
+
+        // pattern 2
+        System.out.println("Pattern 2");
+        for (int i=5; i>0 ; i-- ){
+            for(int j=i ; j>0 ; j--){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+
+        
+
     }
 }
