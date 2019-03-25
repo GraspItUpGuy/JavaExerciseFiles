e64c410e6e7afd0dad61c8b0f89bf32887453fa9
Puneet <birmaan.puneet19@gmail.com>
2019-03-25 16:33:25 +0530

    .
    Signed-off-by: Puneet <birmaan.puneet19@gmail.com>

--- University/Star1/src/Pattern.java
+++ University/Star1/src/Pattern.java
@@ -1,10 +1,23 @@
 public class Pattern {
     public static void main(String[] args){
+        // pattern 4
+        System.out.println("pattern 4");
+
         for(int i=0 ; i<5 ; i++){
             for (int j=0; j<5 ; j++){
                 System.out.print("*" + " ");
             }
             System.out.println("");
         }
+        System.out.println("");
+
+
+        // pattern 5
+//            *
+//           **
+//          ***
+//         ****
+//        *****
+
     }
 }
