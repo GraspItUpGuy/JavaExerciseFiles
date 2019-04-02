e7e1bfd9f387c0bb2c73bd077e6c76d68ba5dc7a
Puneet <birmaan.puneet19@gmail.com>
2019-04-02 23:05:57 +0530

    pattern 11
    Signed-off-by: Puneet <birmaan.puneet19@gmail.com>

---
+++ Projects/Patterns/src/pattern11.java
@@ -0,0 +1,28 @@
+public class pattern11 {
+    public static void main(String[] args){
+
+        int n=5;
+
+
+        for(int i =0 ; i<n; i++){
+            for(int j=0; j<2*n-1 ; j++) {
+                if(  i+j< n-1 ){
+                    System.out.print("   ");
+                }else if( i+j > 2*i + 4 ){
+                    System.out.print("   ");
+                } else if(((i+j) ==n) || ((i+j) ==n+2) || (i+j)==2*n-1 || (i+j)==2*n+1){
+                    System.out.print("   ");
+                }else{
+                    System.out.print(" * ");
+                }
+
+            }
+            System.out.println();
+        }
+
+
+
+
+
+    }
+}
