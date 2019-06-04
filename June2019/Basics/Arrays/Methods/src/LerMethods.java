6dd42e8f0a0cd1780ff4f8a8534ba56a393f0154
Puneet <birmaan.puneet19@gmail.com>
2019-06-04 22:16:35 +0530

    methods
    Signed-off-by: Puneet <birmaan.puneet19@gmail.com>

--- June2019/Basics/Arrays/Methods/src/LerMethods.java
+++ June2019/Basics/Arrays/Methods/src/LerMethods.java
@@ -1,2 +1,18 @@
 public class LerMethods {
+    public static void main(String args[]){
+
+sayHello();
+
+
+    }
+
+    public static void sayHello(){
+        System.out.println("Hello Java !, How are you ???");
+    }
+    /*
+    public    - visible to everyone
+    private   - visible only to class
+    protected - visible to package and all subclasses
+
+  */
 }
