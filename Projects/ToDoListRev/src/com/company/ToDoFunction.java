1c5e77dbb1a312002f79e2de5661d54e2fff6433
Puneet <birmaan.puneet19@gmail.com>
2019-06-18 02:24:41 +0530

    update + search
    Signed-off-by: Puneet <birmaan.puneet19@gmail.com>

--- Projects/ToDoListRev/src/com/company/ToDoFunction.java
+++ Projects/ToDoListRev/src/com/company/ToDoFunction.java
@@ -21,10 +21,29 @@ public class ToDoFunction {
 
     // print the entire list
     public void  PrintTODoList(){
-        System.out.println("Your to-do-list has :  " + toDoList.size() + " number  of elements");
+        System.out.println("Your to-do-list has :  " + toDoList.size() +
+                " number  of elements");
         for(int i=0; i<=toDoList.size(); i++ ){
             System.out.println( i+1 + ":" + toDoList.get(i));
         }
     }
+
+    //update the list
+    public void updateToDo(int index, String list){
+        toDoList.set(index, list);
+        System.out.println("ToDoList updated at position : " + index + 1 );
+    }
+    
+    // search in to-do-list
+    public String findItem(String searchThis){
+       int index = toDoList.indexOf(searchThis);
+       if (index == -1){
+           return null;
+       }else {
+           return toDoList.get(index);
+       }
+    }
+
+
 
 }
