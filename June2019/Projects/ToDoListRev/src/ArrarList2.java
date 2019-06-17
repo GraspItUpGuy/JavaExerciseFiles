b9665d435410b4706755d7879fe6880cf32aa56a
Puneet <birmaan.puneet19@gmail.com>
2019-06-17 21:55:26 +0530

    /
    Signed-off-by: Puneet <birmaan.puneet19@gmail.com>

---
+++ June2019/Projects/ToDoListRev/src/ArrarList2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class ArrarList2 {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        // ToDOLIST Project
+        // CRUD
+
+
+    }
+
+    
+}
